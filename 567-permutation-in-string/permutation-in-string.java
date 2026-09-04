af54bd65c749e75072b197417b13e79185a1fc5e
Aniket Kumar <aniketkrvi777@gmail.com>
2026-09-04 10:58:53 +0530

    Time: 83 ms (12.33%) | Memory: 46.9 MB (17.58%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,54 +1,32 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        int n=s1.length();
+        int m=s2.length();
 
-        if (s1.length() > s2.length()) {
-            return false;
+        if(m<n) return false;   //When s2 is smaller than s1 
+
+        int map1[]=new int[26];
+
+        for(int i=0;i<n;i++){
+            map1[s1.charAt(i)-'a']++;  //elements created for map1
         }
-
-        int[] count = new int[26];
-
-        // Frequency of characters in s1
-        for (char c : s1.toCharArray()) {
-            count[c - 'a']++;
+        for(int i=0;i<=m-n;i++){   //elements created for map2
+            int map2[]=new int[26];
+            for(int j=0;j<n;j++){
+                map2[s2.charAt(i+j)-'a']++;
+            }
+            if(isMatched(map1,map2)){  //for matching map1 and map2
+                return true;  // and if it is matched then return true
+            }
         }
+        return false;  // else false
+    }
 
-        // First window of s2
-        for (int i = 0; i < s1.length(); i++) {
-            count[s2.charAt(i) - 'a']--;
-        }
-
-        // Check first window
-        if (isZero(count)) {
+        private boolean isMatched(int map1[], int map2[]){  //Are these two frequency arrays equal
+            for(int i=0;i<26;i++){
+                if(map1[i]!=map2[i]) return false;
+            }
             return true;
         }
 
-        // Sliding window
-        int left = 0;
-
-        for (int right = s1.length(); right < s2.length(); right++) {
-
-            // Add new character
-            count[s2.charAt(right) - 'a']--;
-
-            // Remove old character
-            count[s2.charAt(left) - 'a']++;
-
-            left++;
-
-            if (isZero(count)) {
-                return true;
-            }
-        }
-
-        return false;
     }
-
-    private boolean isZero(int[] count) {
-        for (int num : count) {
-            if (num != 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
