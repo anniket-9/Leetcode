635dd73bf49f786736b4bb168222337819f39a62
Aniket Kumar <aniketkrvi777@gmail.com>
2026-09-15 19:08:52 +0530

    Time: 5 ms (82.93%) | Memory: 77.6 MB (12.85%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,28 +1,24 @@
 class Solution {
     public int maxArea(int[] height) {
 
-        int i = 0;
-        int j = height.length - 1;
+        //this will help to find the area of container
+        int left=0;
+        int right=height.length-1;
+        int maxArea=0; //keep track of maximum area
 
-        int maxWater = 0;
+        while(left<right){
+            //find min value of 2 heights*width
+            int area=Math.min(height[left], height[right])*(right-left);
 
-        while (i < j) {
+            maxArea=Math.max(area,maxArea);
 
-            int width = j - i;
-            int h = Math.min(height[i], height[j]);
-
-            int water = width * h;
-
-            maxWater = Math.max(maxWater, water);
-
-            // Move the smaller height
-            if (height[i] < height[j]) {
-                i++;
-            } else {
-                j--;
+            if(height[left]< height[right]){ //if left stick is smaller then left++ else right--
+                left++;
+            }
+            else{
+                right--;
             }
         }
-
-        return maxWater;
+        return maxArea;
     }
 }
